64e57c5defd89dd364a84ca08167300770356583
Light <lucgarrett4@gmail.com>
2021-11-29 19:54:28 -0600

    Formatting

--- cipher.java
+++ cipher.java
@@ -3,59 +3,44 @@ class cipher extends RSA{
 
     Boolean prev;
     Boolean mod; 
-
     int lvl; 
     static String roxy; 
     static String raptor; 
 
             cipher(boolean a, boolean b, int c){
-
                 a = prev; 
                 b = mod; 
                 c = lvl; 
-
             }    
 
 
             static String cryptbool(Boolean free, int imp){
-                        
                         roxy = ""; 
-
-
-                        if(imp<5){
+                    if(imp<5){
                             if(free = true){
-                            for(int i = 0; i<=imp; i++){
-                            if(i%2==1){
-                                raptor = roxy + "1";
-                            }
+                                for(int i = 0; i<=imp; i++){
+                                    if(i%2==1){
+                                        raptor = roxy + "1";
+                                    }
                                 
-                            else{
+                                    else{
                                         raptor = roxy + "0";
 
-                            }
+                                    }       
 
-                         }
-                        }
+                                }  
+                            }
                     }
 
 
-                       return raptor ;
+            return raptor ;
             }
             
-
-
-                    
-
     static boolean decryptbool(String str){
            boolean dc = true; 
-           
-    
-            return dc; 
-        
-        
+    return dc;  
     }
     
-
     public static void main(String[] args)  {
 
             System.out.println("hw");
@@ -63,9 +48,4 @@ class cipher extends RSA{
             
             
     }
-
-
-
-
-
 }       
